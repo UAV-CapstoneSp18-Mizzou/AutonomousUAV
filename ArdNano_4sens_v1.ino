8d3f42aa1c95578af7d874e6b2375c2314a57b52
Blake Bremer <37942227+BlakeB7@users.noreply.github.com>
2018-04-26 20:31:03 -0500

    Filtered Arduino
    Updated to the version last tested on 4.26.18 that uses a low pass filter to cut out the sensor noise.

--- ArdNano_4sens_v1.ino
+++ ArdNano_4sens_v1.ino
@@ -1,57 +1,70 @@
-// Define pins to be used. On Arduino "D5, D6, D7, D7"
-const int pwPin1 = 5;
-const int pwPin2 = 6;
-const int pwPin3 = 7;
-const int pwPin4 = 8;
+#include <MegunoLink.h>
+
+// Define pins to be used. On Arduino "D9, D10, D11, D12"
+const int pwPin1 = 9; //North
+const int pwPin2 = 10; //South
+const int pwPin3 = 11;  //East
+const int pwPin4 = 12; //West
 
 //variables to store values
-long pulse1, pulse2, pulse3, pulse4, cm1, cm2, cm3, cm4;
+long pulse1, pulse2, pulse3, pulse4, cm1, cm2, cm3, cm4, cm_1, cm_2, cm_3, cm_4;
 
 void setup()
 {
-  Serial.begin(9600); // Begins serial and defines baud rate. Make sure baud is same in Python
-  
-  pinMode(pwPin1, INPUT); // Sets each pin as input only - arduino doesn't write out
-  pinMode(pwPin2, INPUT);
-  pinMode(pwPin3, INPUT);
-  pinMode(pwPin4, INPUT);
+  //This opens up a serial connection to shoot the results back to the PC console
+  Serial.begin(9600);
+
+
 }
 
 void loop()
 {
-
-  //Now reading the physical pwm values
+  pinMode(pwPin1, INPUT);
+  pinMode(pwPin2, INPUT);
+  pinMode(pwPin3, INPUT);
+  pinMode(pwPin4, INPUT);
+  //Used to read in the pulse that is being sent by the MaxSonar device
+  //Pulse Width respresentation with a scale factor of 147 uS per Inch
   pulse1 = pulseIn(pwPin1, HIGH);
   pulse2 = pulseIn(pwPin2, HIGH);
   pulse3 = pulseIn(pwPin3, HIGH);
   pulse4 = pulseIn(pwPin4, HIGH);
 
-  //147uS per inch for Maxbotic
-
-  cm1 = pulse1 / 147 * 2.54; // Converts pulse width to cm
+  cm1 = pulse1 / 147 * 2.54; //converts pulse width to cm
   cm2 = pulse2 / 147 * 2.54;
   cm3 = pulse3 / 147 * 2.54;
   cm4 = pulse4 / 147 * 2.54;
- 
-  Serial.print("#"); //Start symbol
-  
-  Serial.print(cm1, DEC); // Pin #5
-  Serial.print(".");
-  
-  Serial.print(cm2, DEC); // Pin #6
-  Serial.print(".");
-  
-  Serial.print(cm3, DEC); // Pin #7
-  Serial.print(".");
-  
-  Serial.print(cm4); // Pin #8
-  
-  Serial.print("$");  //End symbol
-    
-  Serial.println(); // Important to have new line since Python reads by line
-  delay(100);
 
+  cm_1 = 0.8529*cm_1+0.1471*cm1; //filtered pulse
+  cm_2 = 0.8529*cm_2+0.1471*cm2;
+  cm_3 = 0.8529*cm_3+0.1471*cm3;
+  cm_4 = 0.8529*cm_4+0.1471*cm4;
+
+  Serial.print("#");
+  
+  Serial.print(cm_1, DEC); // Pin #9
+  Serial.print(".");
+
+  Serial.print(cm_2, DEC); // Pin #10
+  Serial.print(".");
+
+  Serial.print(cm_3, DEC); // Pin #11
+  Serial.print(".");
+
+  Serial.print(cm_4, DEC); // Pin #12
+  Serial.print("$");
+
+  Serial.println();
+
+//  TimePlot Plot;
+//  Plot.SendData("Filtered cm1", cm_1);
+//  //Plot.SendData("Raw1", cm1);
+//  Plot.SendData("Filtered cm2", cm_2);
+//  //Plot.SendData("Raw2", cm2);
+//  Plot.SendData("Filtered cm3", cm_3);
+//  //Plot.SendData("Raw3", cm3);
+//  Plot.SendData("Filtered cm4", cm_4);
+//  //Plot.SendData("Raw4", cm4);
+
+  delay(500); //100ms delay
 }
-
-//comment
-//hi, lets fly.
